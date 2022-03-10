cda44abce31b41457827d4b3d48e22ef79e96b98
Philipp Winter <phw@brave.com>
2022-03-10 14:26:59 -0800

    Make example take endpoints from env vars.

--- example/main.go
+++ example/main.go
@@ -3,22 +3,51 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/brave-experiments/viproxy"
+	"github.com/mdlayher/vsock"
 )
 
-func resolveTCPAddr(addr string) *net.TCPAddr {
-	a, err := net.ResolveTCPAddr("tcp", addr)
-	if err != nil {
-		log.Fatalf("Failed to resolve TCP address: %s", err)
+func parseAddr(rawAddr string) net.Addr {
+	var addr net.Addr
+	var err error
+
+	addr, err = net.ResolveTCPAddr("tcp", rawAddr)
+	if err == nil {
+		return addr
 	}
-	return a
+
+	// We couldn't parse the address, so we must be dealing with AF_VSOCK.  We
+	// expect an address like 3:8080.
+	fields := strings.Split(rawAddr, ":")
+	if len(fields) != 2 {
+		log.Fatal("Looks like we're given neither AF_INET nor AF_VSOCK addr.")
+	}
+	cid, err := strconv.Atoi(fields[0])
+	if err != nil {
+		log.Fatal("Couldn't turn CID into integer.")
+	}
+	port, err := strconv.Atoi(fields[1])
+	if err != nil {
+		log.Fatal("Couldn't turn port into integer.")
+	}
+
+	return &vsock.Addr{ContextID: uint32(cid), Port: uint32(port)}
 }
 
 func main() {
+	// E.g.: IN_ADDR=127.0.0.1:8080 OUT_ADDR=3:8080 go run main.go
+	rawInAddr, rawOutAddr := os.Getenv("IN_ADDR"), os.Getenv("OUT_ADDR")
+	if rawInAddr == "" || rawOutAddr == "" {
+		log.Fatal("Environment variables IN_ADDR and OUT_ADDR not set.")
+	}
+
 	tuple := &viproxy.Tuple{
-		InAddr:  resolveTCPAddr("127.0.0.1:8080"),
-		OutAddr: resolveTCPAddr("127.0.0.1:443"),
+		InAddr:  parseAddr(rawInAddr),
+		OutAddr: parseAddr(rawOutAddr),
 	}
 	p := viproxy.NewVIProxy([]*viproxy.Tuple{tuple})
 	p.Start()
